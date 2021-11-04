abda5c6c5dd016aac70d14635beddc8f21396d89
Christopher Terrano <mterrano1@yahoo.com>
2021-11-04 12:38:22 -0500

    commit tree 819ae429b9fc1efafc1ed6e48b1752ad9672df17

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'ahole';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not an ahole';
+    return leastFavoriteCustomer;
+}
